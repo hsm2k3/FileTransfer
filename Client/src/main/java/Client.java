fe9b2b4d408f6654791919d9bfdac159ead9e3f3
hsm2k3 <pachshot@yahoo.com>
2019-09-13 02:08:53 -0400

    task copmlete: file transfer complete bug: copied file contains invalid characters

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -1,15 +1,28 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String [] args ) throws IOException {
-        byte bytes[] = new byte[2002];
-        Socket socket = new Socket("localhost", 4333);
-        InputStream inputStream = socket.getInputStream();
-        FileOutputStream fileOutputStream = new FileOutputStream("/media/alex/easystore/success_testTransfer.txt");
-        inputStream.read(bytes, 0, bytes.length);
-        fileOutputStream.write(bytes, 0, bytes.length);
+    public static void main(String [] args ){
+
+        String fileName = "/media/alex/easystore/testTransfer.txt";
+        try
+        {
+            File file = new File(fileName);
+            Socket socket = new Socket("localhost", 35103);
+            byte[] byteArray = new byte[(int)file.length()];
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+            bufferedInputStream.read(byteArray, 0, byteArray.length);
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(byteArray, 0, byteArray.length);
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,18 +1,53 @@
-import java.io.FileInputStream;
+
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server extends Thread {
+    public static final int PORT = 4333;
+    public static final int BUFFER_SIZE = 626;
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(35103);
+            System.out.println("listening on port: " + serverSocket.getLocalPort());
+            while(true)
+            {
+                Socket socket = serverSocket.accept();
+                saveFile(socket);
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    private void saveFile(Socket socket) throws IOException {
+        InputStream inputStream = socket.getInputStream();
+        FileOutputStream fileOutputStream = new FileOutputStream("/home/alex/Success_testTransfer.txt");
+
+        byte[] byteArray = new byte[1024];
+        System.out.println("Reading file from server.");
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        int bytesRead;
+        while((bytesRead = inputStream.read(byteArray)) != -1 )
+        {
+            bufferedOutputStream.write(byteArray);
+        }
+
+        bufferedOutputStream.close();
+        System.out.println("Writing file complete.");
+    }
     public static void main (String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(4333);
-        Socket socket = serverSocket.accept();
-        FileInputStream fileInputStream = new FileInputStream("/media/alex/easystore/testTransfer.txt");
-        byte bytes [] = new byte[2002];
-        fileInputStream.read(bytes, 0 ,bytes.length);
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write(bytes, 0, bytes.length);
+        new Server().start();
 
     }
 }
